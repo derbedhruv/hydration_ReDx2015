6f1c3c45ff5b97589d184bb32003d8d709bc6e44
divya <divya.ramnath13@gmail.com>
2015-01-27 14:02:59 +0530

    added modified Arduino code for DC extraction

--- Divya/DC_extraction.py
+++ Divya/DC_extraction.py
@@ -4,20 +4,7 @@ import numpy as np
 from pylab import *
 from numpy import *
 
-from scipy.signal import butter, lfilter
-from scipy.signal import freqs
 
-def butter_lowpass(cutOff, fs, order=5):
-    nyq = 0.5 * fs
-    normalCutoff = cutOff / nyq
-    b, a = butter(order, normalCutoff, btype='low', analog = True)
-    return b, a
-
-def butter_lowpass_filter(data, cutOff, fs, order=4):
-    b, a = butter_lowpass(cutOff, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
-    
 filename="C:/Users/Divya/Documents/RedX/hydration_ReDx2015/Divya/Sample_data_without_headers/20140522-0002_2.txt"
 lines= [line.rstrip('\n').split('	') for line in open(filename,"r")]
 
@@ -50,16 +37,6 @@ o = data - filtered_value1
     
 print ("Plotting output")
 
-#print filtered_value
-
-#plt.subplots(1,1)
-
-cutOff = 2*math.pi*0.5 #23.1 #cutoff frequency in rad/s
-fs = 2*math.pi*50#188.495559 #sampling frequency in rad/s
-order = 4 #order of filter
-
-#fil_value = butter_lowpass_filter(data, cutOff, fs, order)
-#plt.plot(time,fil_value,'b')
 
 
 plot(time,data,'c')
