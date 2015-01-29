7c55d29941eb2d951cde44c7e9d5fb73494a2017
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-01-29 08:39:30 +0530

    reading in the data, need to integrate the part that processes it

--- Dhruv/oscilloscope_read.py
+++ Dhruv/oscilloscope_read.py
@@ -1,3 +1,19 @@
 # we;re going to extrat data from the oscilloscope and put it into proper variables for monitoring later
+import numpy, matplotlib
 
 f = open('data.txt')	# open the file saved by the oscilloscope
+header_lines = 7
+
+data = f.readlines()
+
+chanA = []
+chanB = []
+
+for i in range(header_lines, len(d)):
+  dline = data[i].split(",")
+  
+  # put the data in
+  chanA.append((int)dline[0].strip())
+  chanB.append((int)dline[1].strip())
+  
+# Now we have the data in the form of 2 channels .. needs to be processed
